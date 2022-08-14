6e59215966dff0c707d2eb37ca0c8d082d280e5b
John Gavin <john.b.gavin@gmail.com>
2022-08-14 21:18:39 +0100

    stash

--- _targets.R
+++ _targets.R
@@ -1,10 +1,10 @@
 
 
 system.time(
-  source(here::here('R', 'tar_plans', 'plan_preamble.R'),
-    local = TRUE) )
+  source(here::here('R', 'tar_plans', 'plan_preamble.R'), # nolint
+    local = TRUE))
 
-source(here::here('R', 'tar_plans', 'tar_plan_ccxt.R'), local = TRUE)
+source(here::here('R', 'tar_plans', 'tar_plan_ccxt.R'), local = TRUE) # nolint
 # source(here::here('R', 'tar_plans', 'plan_calibrate_act_v_exp.R'), local = TRUE)
 # source(here::here('R', 'tar_plans', 'plan_fbref_get_data.R'), local = TRUE)
 
@@ -12,6 +12,5 @@ source(here::here('R', 'tar_plans', 'tar_plan_ccxt.R'), local = TRUE)
 list2(
   plan_ccxt
   # plan_random_forrest ,
-	# , plan_fbref_get_data
+  # , plan_fbref_get_data
 )[[1]]
-
